9a0b5e7e420d21911073a41ab5c4cd94e7c75bc4
John Glebov <egoji@mail.ru>
2021-10-23 22:16:16 +0300

    push

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return(
+        <footer className="page-footer orange darken-4">            
+            <div className="footer-copyright">
+                <div className="container">
+                    
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export {Footer}
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,9 @@
+function Preloader(){
+    return (
+        <div className="progress orange darken-4">
+            <div className="indeterminate purple darken-4"></div>
+        </div>
+    )
+}
+
+export {Preloader}
